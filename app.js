4b4b253625dc72b2ae332167f738f8630b0b8664
Gabriel Bocchio <bocchio.gabriel@gmail.com>
2023-02-22 11:08:34 +0100

    actualize puerto para RENDER en app.js y corregí scripts del package json

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require("path");
 app.use(express.static(path.resolve(__dirname, "./public")))
 
 
-app.listen(3000, ()=> console.log("Server corriendo en puerto 3000"))
+/* app.listen(3000, ()=> console.log("Server corriendo en puerto 3000")) */
+const port = process.env.PORT || 3001;
+app.listen(port, ()=> console.log("Server corriendo en puerto ${port}"));
+
 
 app.get("/", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/home.html")))
 
